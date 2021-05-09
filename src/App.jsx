d0ea02ef246fd2ed36dd31d1ca7ee0cdee784735
mitchelst <mitchel@bendingtherules.nl>
2021-05-09 12:53:13 +0200

    add: intro

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,24 @@
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+import AppHeader from "./components/AppHeader";
+import Dashboard from "./components/Dashboard";
+import Login from "./components/Login";
+
 export default function App() {
-  return <></>;
+  return (
+    <>
+      <Router>
+        <AppHeader />
+        <Switch>
+          <Route path="/login" component={Login} />
+          <Route path="/dashboard" component={Dashboard} />
+          <Redirect to="/login" />
+        </Switch>
+      </Router>
+    </>
+  );
 }
---
+++ src/components/AppHeader.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function AppHeader() {
+  return <>AppHeader</>;
+}
