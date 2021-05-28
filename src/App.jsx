42df49c7da1a9ab6e09173804efe18bd6c16371c
mitchelst <mitchel@bendingtherules.nl>
2021-05-28 20:23:45 +0200

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,26 @@
+import styled from "styled-components";
+
 export default function App() {
-  return <></>;
+  return (
+    <>
+      <StyledButton primary>I want this to be red</StyledButton>
+      <StyledLargeButton>Large button</StyledLargeButton>
+    </>
+  );
 }
+
+const StyledButton = styled.button`
+  background-color: ${(props) => (props.primary ? "red" : "green")};
+
+  &:hover {
+    background-color: purple;
+  }
+
+  @media (max-width: 500px) {
+    background-color: yellow;
+  }
+`;
+
+const StyledLargeButton = styled(StyledButton)`
+  padding: 1em;
+`;
