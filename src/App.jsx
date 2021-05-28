fc1518beee30c3b9cf563e5fe9773709722191cb
mitchelst <mitchel@bendingtherules.nl>
2021-05-28 19:15:42 +0200

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,13 @@
+import "./App.scss";
+import HeaderComponent from "./HeaderComponent";
+
 export default function App() {
-  return <></>;
+  const headerText = "This is a header";
+
+  return (
+    <>
+      <h1 className={headerText.length > 5 ? "header" : null}>{headerText}</h1>
+      <HeaderComponent />
+    </>
+  );
 }
---
+++ src/HeaderComponent.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./HeaderComponent.scss";
+
+export default function HeaderComponent() {
+  return (
+    <>
+      <h1 className="header">HeaderComponent</h1>
+    </>
+  );
+}
