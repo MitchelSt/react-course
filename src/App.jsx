181f1a570d71a93e0e51d01cd485ea39941f58f0
mitchelst <mitchel@bendingtherules.nl>
2021-05-30 12:58:08 +0200

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,34 @@
+import { useState } from "react";
+
 export default function App() {
-  return <></>;
+  const [accountBalance, setAccountBalance] = useState(5000);
+  const [transferAmount, setTransferAmount] = useState(0);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    setAccountBalance((prev) => prev - transferAmount);
+    setTransferAmount(0);
+  };
+
+  return (
+    <>
+      <h1>Account</h1>
+
+      <p>Current account balance: {accountBalance}</p>
+
+      <p>Send money to: John Doe</p>
+      <form onSubmit={onSubmit}>
+        <label htmlFor="transferAmount">Transfer Amount</label>
+        <br />
+        <input
+          min="0"
+          type="number"
+          id="transferAmount"
+          value={transferAmount}
+          onChange={(e) => setTransferAmount(e.target.value)}
+        />
+        <button type="submit">Send</button>
+      </form>
+    </>
+  );
 }
