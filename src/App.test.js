6bbcad68725813c5df5ae40c291e17456d0678e8
mitchelst <mitchel@bendingtherules.nl>
2021-05-30 15:50:57 +0200

    wip

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-
-test('if transferToPerson name prop is passed, render the name', () => {
-    render(<App transferToPerson="Michael Jackson" />);
-
-    expect(screen.getByText(/send money to: michael jackson/i)).toBeInTheDocument();
-});
