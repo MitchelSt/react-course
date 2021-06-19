e0f841537e619f05346c2c07208ff02442615e8d
mitchelst <mitchel@bendingtherules.nl>
2021-06-19 11:25:03 +0200

    wip

--- src/pages/Chart/Chart.jsx
+++ src/pages/Chart/Chart.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styles from "./Chart.module.css";
 
 export default function Chart() {
   return (
     <>
-      Chart
+      <p styles={styles.chart}>Chart</p>
       <br />
       <Link to="/dashboard">Go to Dashboard</Link>
     </>
