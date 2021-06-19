fe59782cfbff02be3df67a9564e7c08e1199bc4f
mitchelst <mitchel@bendingtherules.nl>
2021-06-19 23:16:49 +0200

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from "./pages/Header/Header";
 import Dashboard from "./pages/Dashboard/Dashboard";
-import Chart from "./pages/Chart/Chart";
+// import Chart from "./pages/Chart/Chart";
+import { lazy, Suspense } from "react";
+
+const Chart = lazy(() => import("./pages/Chart/Chart"));
 
 export default function App() {
   return (
@@ -9,7 +12,9 @@ export default function App() {
       <Header />
       <Router>
         <Route exact path="/dashboard" component={Dashboard} />
-        <Route exact path="/chart" component={Chart} />
+        <Suspense fallback={<p>Loading...</p>}>
+          <Route exact path="/chart" component={Chart} />
+        </Suspense>
       </Router>
     </>
   );
