bee243a5c8b1717476ca39932d68a935a1c32f0c
mitchelst <mitchel@bendingtherules.nl>
2021-06-19 11:04:13 +0200

    wip

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./Dashboard.module.css";
+import styles from "./Dashboard.module.css";
 
 export default function Dashboard() {
   return (
     <>
-      Dashboard
+      <p styles={styles.dashboard}>Dashboard</p>
       <br />
       <Link to="/chart">Go to Chart</Link>
     </>
